7274d18b3fe5a60b3e48304f08c236488cad33b2
MasamiYamate <m.yamate@mail.yamatte.net>
2019-08-23 22:26:55 +0900

    Initial Commit

---
+++ AirImageUtility/AirImageUtility.h
@@ -0,0 +1,19 @@
+//
+//  AirImageUtility.h
+//  AirImageUtility
+//
+//  Created by MasamiYamate on 2019/08/23.
+//  Copyright © 2019 net.yamatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AirImageUtility.
+FOUNDATION_EXPORT double AirImageUtilityVersionNumber;
+
+//! Project version string for AirImageUtility.
+FOUNDATION_EXPORT const unsigned char AirImageUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirImageUtility/PublicHeader.h>
+
+
