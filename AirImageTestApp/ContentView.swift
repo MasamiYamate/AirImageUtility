762ada629e4084e5307bc7402c0d24ea97108628
MasamiYamate <m.yamate@mail.yamatte.net>
2019-10-10 23:14:25 +0900

    SwiftUIのデモアプリを追加

---
+++ AirImageTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirImageTestApp
+//
+//  Created by MasamiYamate on 2019/10/10.
+//  Copyright © 2019 net.yamatte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
